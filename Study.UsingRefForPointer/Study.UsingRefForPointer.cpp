7e91f1c35d09e69d64d3ad4a4a3cb3ece9d92bc5
Gutsik228 <igordudkov22@gmail.com>
2023-09-16 09:57:51 +0500

    Add Study.UsingRefForPointer

---
+++ Study.UsingRefForPointer/Study.UsingRefForPointer.cpp
@@ -0,0 +1,43 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int a = 5;
+    int *pa = &a;
+    int& RefA = a;
+    
+    cout << "a = " << a << endl;
+    cout << "*pa = " << *pa << endl;
+    cout << "RefA = " << RefA << endl;
+
+    cout << endl;
+
+    a = 7;
+
+
+    cout << "a = " << a << endl;
+    cout << "*pa = " << *pa << endl;
+    cout << "RefA = " << RefA << endl;
+
+    cout << endl;
+
+    *pa = 8;
+    
+
+    cout << "a = " << a << endl;
+    cout << "*pa = " << *pa << endl;
+    cout << "RefA = " << RefA << endl;
+
+    cout << endl;
+
+    RefA = 10;
+
+    cout << "a = " << a << endl;
+    cout << "*pa = " << *pa << endl;
+    cout << "RefA = " << RefA << endl;
+}
+
