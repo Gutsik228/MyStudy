5bbaca0ccfb7e04c9b47bbf30166a7200c1013f5
Gutsik228 <igordudkov22@gmail.com>
2023-09-16 10:41:43 +0500

    Add Study.TemplateFuncWithReference

---
+++ Study.TemplateFuncWithReference/Study.TemplateFuncWithReference.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+template <typename T>
+void SwapValue(T &a, T &b)
+{
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+
+int main()
+{
+    int a = 6;
+    int b = 3;
+
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+
+    SwapValue(a, b);
+    cout << "SwapValue" << endl;
+
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+}
+
