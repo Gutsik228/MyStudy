557e67d17232d6fdbeb8afedace5002ce4620a22
Gutsik228 <igordudkov22@gmail.com>
2023-09-16 09:24:11 +0500

    Добавьте файлы проекта.

---
+++ SwapNumbersByFuncAndReference/SwapNumbersByFuncAndReference.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void reverse(int* pa, int* pb)
+{
+	int rev;
+	rev = *pb;
+	*pb = *pa;
+	*pa = rev;
+
+}
+
+void main()
+{
+	int a = 5;
+	int b = 6;
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+
+	reverse(&a, &b);
+	cout << "reverse" << endl;
+
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+}
