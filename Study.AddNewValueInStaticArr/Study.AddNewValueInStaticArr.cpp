ae140234785db450dd49b2e82e1616df3e03e399
Gutsik228 <igordudkov22@gmail.com>
2023-10-07 18:15:19 +0500

    Add Study.AddNewValueInStaticArr

--- Study.AddNewValueInStaticArr/Study.AddNewValueInStaticArr.cpp
+++ Study.AddNewValueInStaticArr/Study.AddNewValueInStaticArr.cpp
@@ -1,29 +1,41 @@
-﻿
-
 #include <iostream>
 #include <cstdlib>
 using namespace std;
 
-void push_back(int* arr, int& size, int value);
+void push_back(int *&arr, int& size, int value);
 void AddRandNum(int* const arr, const int size);
 void ShowArr(int* const arr, const int size);
 
 int main()
 {
-	int size = 5;
-	int* arr = new int[size];
+    int size = 5;
+    int* arr = new int[size];
     AddRandNum(arr, size);
 
     ShowArr(arr, size);
+    cout << endl;
 
     cout << "=============================" << endl;
 
     push_back(arr, size, 10);
 
+    ShowArr(arr, size);
+    cout << endl;
+
 }
 
-void push_back(int* arr, int* size, int value)
+void push_back(int *&arr, int& size, int value)
 {
+    int* NewArr = new int[size + 1];
+    for(int i = 0; i < size; i++)
+    {
+        NewArr[i] = arr[i]; 
+    }
+    NewArr[size] = value;
+    size++;
+
+    delete[] arr;
+    arr = NewArr;
 
 }
 
@@ -42,5 +54,3 @@ void ShowArr(int* const arr, const int size)
         cout << arr[i] << " ";
     }
 }
-
-
