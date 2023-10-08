9089d80830f552a19a478b591e50c00953cebb4b
Gutsik228 <igordudkov22@gmail.com>
2023-10-08 13:09:28 +0500

    Study.ConcCharAndString

--- Study. ConcCharAndString/Study. ConcCharAndString.cpp
+++ Study. ConcCharAndString/Study. ConcCharAndString.cpp
@@ -1,5 +1,3 @@
-﻿
-
 #include <iostream>
 #include <string>
 using namespace std;
@@ -9,25 +7,26 @@ int main()
 	char ch[255] = "String is good";
 	char ch2[255] = "or";
 	char ch3[255] = "Char is good";
-	char resultChar[255];
+	char resultChar[255] = {""};
 	
-	strcat(resultChar, ch);
-	strcat(resultChar, ch2);
-	strcat(resultChar, ch3);
+	cout << "Using char and strcat" << endl;
+	strcat_s(resultChar, ch);
+	strcat_s(resultChar, ch2);
+	strcat_s(resultChar, ch3);
 
 	cout << resultChar << endl;
 
+
+	cout << endl << endl;
 
 	cout << "Using string" << endl;
 	string str1 = "Hello";
 	string str2 = "World";
 	string resultString{};
-	
+
+	resultString = str1 + " " + str2;
 	cout << resultString << endl;
 
 	return 0;
 }
-
-
-
 
