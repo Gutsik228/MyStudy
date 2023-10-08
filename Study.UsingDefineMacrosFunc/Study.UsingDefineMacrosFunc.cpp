4ff98efeac2455d3f570b05c96c85c70bbb45bf8
Gutsik228 <igordudkov22@gmail.com>
2023-10-08 14:47:22 +0500

    add Study.UsingDefineMacrosFunc

---
+++ Study.UsingDefineMacrosFunc/Study.UsingDefineMacrosFunc.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#define PI 3.14
+#define Prod(a, b)((a) * (b))
+using namespace std;
+
+int main()
+{
+	int a = 5; 
+	int b = 10;
+	
+	cout << a << " " << b << endl;
+
+	cout << "Pi = " << PI << endl;
+
+	cout << "Prod two nums = " << Prod(a, b);
+}
